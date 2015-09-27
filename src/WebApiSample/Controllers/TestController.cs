fdb3aa803d4c83b36be6ae68405e3c304c87c7cc
Sujithkumarreddy <sujithsinbox@gmail.com>
2015-09-27 19:15:28 +0530

    Update TestController.cs

--- src/WebApiSample/Controllers/TestController.cs
+++ src/WebApiSample/Controllers/TestController.cs
@@ -1,12 +1,11 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
 using System.Xml;
-//using MvcApplication1.Models;
 using System.Xml.Linq;
-using System.IO;
+
 
 namespace WebApiSample.Controllers
 {
