d7156ceffda05d570b6e4b49640dc7120d79e93c
Sujithkumarreddy <sujithsinbox@gmail.com>
2015-09-30 02:38:59 -0700

    Seeder REmoved

--- src/WebApiSample/Initializer/Seeder.cs
+++ src/WebApiSample/Initializer/Seeder.cs
@@ -6,17 +6,17 @@ using WebApiSample.Models;
 
 namespace WebApiSample.Initializer
 {
-    public class Seeder
-    {
-        public static List<Product> lstProducts { get; set; }
-        public static List<CreateAdd> lstAdds { get; set; }
-        public static List<AddsHistory> lstAddsHistory { get; set; }
+    //public class Seeder
+    //{
+    //    public static List<Product> lstProducts { get; set; }
+    //    public static List<CreateAdd> lstAdds { get; set; }
+    //    public static List<AddsHistory> lstAddsHistory { get; set; }
 
-        public static void intialize()
-        {
-            lstProducts = DataFeeder.getProducts();
-            lstAdds = DataFeeder.getActiveAdds();
-            lstAddsHistory = DataFeeder.getAddsHistory();
-        }
-    }
+    //    public static void intialize()
+    //    {
+    //        lstProducts = DataFeeder.getProducts();
+    //        lstAdds = DataFeeder.getActiveAdds();
+    //        lstAddsHistory = DataFeeder.getAddsHistory();
+    //    }
+    //}
 }
