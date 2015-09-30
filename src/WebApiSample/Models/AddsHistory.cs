3753cb68964d15b1f8064a3c58dcac0c5604c22c
Sujithkumarreddy <sujithsinbox@gmail.com>
2015-09-30 01:54:43 -0700

    Added Models

---
+++ src/WebApiSample/Models/AddsHistory.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApiSample.Models
+{
+    public class AddsHistory
+    {
+        public int ID { get; set; }
+        public string AgeGroup { get; set; }
+        public string Gender { get; set; }
+        public string Region { get; set; }     
+        public int Views { get; set; }
+    }
+}
